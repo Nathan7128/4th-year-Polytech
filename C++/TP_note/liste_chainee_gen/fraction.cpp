1e877c3532caea2bdc2781053f2316444d90cfd0
Nathan Talbot <nathan.talbot28@gmail.com>
2024-11-12 12:07:06 +0100

    tp noté bdd

---
+++ C++/TP_note/liste_chainee_gen/fraction.cpp
@@ -0,0 +1,137 @@
+//définition des méthodes de la classe fraction
+
+
+
+//import libraries
+#include "fraction.h"
+
+
+
+Fraction::Fraction(int num, int deno) {
+	if (deno != 0) {
+		m_numerateur = num;
+		m_denominateur = deno;
+	}
+	else {
+		throw ExceptionDenoNul("Erreur d'initialisation : denominateur nul");
+	}
+}
+
+int Fraction::getNum() {
+	return m_numerateur;
+}
+
+int Fraction::getDeno() {
+	return m_denominateur;
+}
+
+void Fraction::inverse() {
+	if (m_numerateur != 0) {
+		int temp = m_numerateur;
+		m_numerateur = m_denominateur;
+		m_denominateur = temp;
+	}
+	else {
+		throw ExceptionDenoNul("Erreur d'inversion : impossible d'inverser une fraction nulle");
+	}
+}
+
+int pgcd(int a, int b) {
+	int r = b;
+	while (a % b != 0) {
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return r;
+}
+
+void Fraction::simplifier() {
+	int vpgcd = pgcd(m_numerateur, m_denominateur);
+	m_numerateur /= vpgcd;
+	m_denominateur /= vpgcd;
+	if (m_denominateur < 0) {
+		m_denominateur *= -1;
+		m_numerateur *= -1;
+	}
+}
+
+Fraction operator+(const Fraction& f1, const Fraction& f2) {
+	int deno_commun, somme_num;
+	deno_commun = f1.m_denominateur * f2.m_denominateur;
+	somme_num = f1.m_numerateur * f2.m_denominateur + f2.m_numerateur * f1.m_denominateur;
+	Fraction f(somme_num, deno_commun);
+	f.simplifier();
+	return f;
+}
+
+Fraction operator-(const Fraction& f1, const Fraction& f2) {
+	int deno_commun, soustr_num;
+	deno_commun = f1.m_denominateur * f2.m_denominateur;
+	soustr_num = f1.m_numerateur * f2.m_denominateur - f2.m_numerateur * f1.m_denominateur;
+	Fraction f(soustr_num, deno_commun);
+	f.simplifier();
+	return f;
+}
+
+Fraction operator*(const Fraction& f1, const Fraction& f2) {
+	Fraction f(f1.m_numerateur * f2.m_numerateur, f1.m_denominateur * f2.m_denominateur);
+	f.simplifier();
+	return f;
+}
+
+Fraction operator/(const Fraction& f1, const Fraction& f2) {
+	if (f2.m_numerateur != 0) {
+		Fraction f(f1.m_numerateur * f2.m_denominateur, f2.m_numerateur * f1.m_denominateur);
+		f.simplifier();
+		return f;
+	}
+	else {
+		throw ExceptionDivZero("Erreur de division : impossible de diviser par une fraction nulle");
+	}
+}
+
+bool operator==(const Fraction& f1, const Fraction& f2) {
+	Fraction f1_simp = f1, f2_simp = f2;
+	f1_simp.simplifier();
+	f2_simp.simplifier();
+	return (f1_simp.m_numerateur == f2_simp.m_numerateur and f1_simp.m_denominateur == f2_simp.m_denominateur);
+}
+
+bool operator!=(const Fraction& f1, const Fraction& f2) {
+	return !(f1 == f2);
+}
+
+bool operator<(const Fraction& f1, const Fraction& f2) {
+	return (f1.m_numerateur * f2.m_denominateur < f2.m_numerateur * f1.m_denominateur);
+}
+
+bool operator>(const Fraction& f1, const Fraction& f2) {
+	return (f1.m_numerateur * f2.m_denominateur > f2.m_numerateur * f1.m_denominateur);
+}
+
+bool operator<=(const Fraction& f1, const Fraction& f2) {
+	return (f1 == f2 or f1 < f2);
+}
+
+bool operator>=(const Fraction& f1, const Fraction& f2) {
+	return (f1 == f2 or f1 > f2);
+}
+
+ostream& operator<<(ostream& f, const Fraction& frac) {
+	if (frac.m_numerateur == 0 or frac.m_denominateur == 1) {
+		f << frac.m_numerateur;
+	}
+	else {
+		f << frac.m_numerateur << " / " << frac.m_denominateur;
+	}
+	return f;
+}
+
+istream& operator>>(istream& f, Fraction& frac) {
+	cout << "Numerateur : ";
+	f >> frac.m_numerateur;
+	cout << "Denominateur : ";
+	f >> frac.m_denominateur;
+	return f;
+}
---
+++ C++/TP_note/liste_chainee_gen/liste_chainee_gen.h
@@ -0,0 +1,439 @@
+//implémentation de la classe Liste chainée générique
+
+
+
+//chiens de garde
+#ifndef _LISTE_CHAINEE_GEN_
+#define _LISTE_CHAINEE_GEN_
+
+
+
+//importation des bibliothèques
+#include <iostream>
+#include <string>
+#include "fraction.h"
+using namespace std;
+
+
+
+//définition du type noeud de la liste chainée
+template <typename T>
+struct noeud {
+	T valeur;
+	noeud* suivant;
+};
+
+
+
+//classe Liste Chainée : permet de stocker des entiers dans une liste chainée
+template <typename T>
+class liste_chainee {
+private:
+	noeud* m_debut; /*tête de la liste*/
+
+public:
+	liste_chainee(); /*constructeur par défaut*/
+	liste_chainee(const liste_chainee& l); /*constructeur de recopie*/
+	~liste_chainee(); /*destructeur*/
+	T front(); /*retourne le premier élément de la liste*/
+	T back(); /*retourne le dernier élément de la liste*/
+	void push_front(T elt); /*ajoute un élément au début de la liste*/
+	void push_back(T elt); /*ajoute un élément à la fin de la liste*/
+	void pop_front(); /*supprime le premier élément de la liste*/
+	void pop_back(); /*supprimer le dernier élement de la liste*/
+	T size(); /*renvoie le nombre d’éléments stockés dans la liste*/
+	void insert(int i, T elt); /*Le 1er élément de la liste (s'il existe) est à la position i = 1
+								  Si la liste contient par exemple 3 éléments, il n'est pas possible d'ajouter un élément en position 4 :
+								  il faut effectuer un push_back*/
+	void erase(int i); /*supprime le ième élement de la liste*/
+	bool empty(); /*renvoie vrai si la liste est vide, faux sinon*/
+	void clear(); /*vide la liste*/
+	void remove(T elt); /*supprime de la liste tous les éléments égaux à elt*/
+	void sort(); /*trie la liste par ordre croissant*/
+	liste_chainee& operator=(const liste_chainee& l); /*attribrue les valeurs d'une liste à une autre liste*/
+
+	template <typename T>friend noeud* sep_list(noeud* debut); /*permet de séparer la liste chainée en 2 pour le merge sort :
+															   renvoie un pointeur vers la deuxième moitié de la liste*/
+	template <typename T>friend noeud* merge(noeud* debut, noeud* fin); /*fusionne 2 listes chainées passées en paramètre*/
+	template <typename T>friend noeud* merge_sort(noeud* debut); /*permet de trier récursivement des sous parties de la liste,
+																 puis de fusionner ces sous parties*/
+	template <typename T>friend ostream& operator<<(ostream& f, const liste_chainee& l); /*affiche la liste*/
+	template <typename T>friend liste_chainee operator+(const liste_chainee& l1, const liste_chainee& l2); /*concatène 2 listes*/
+};
+
+
+
+//classe exception liste vide
+class ExceptionListeVide {
+public:
+	string m_message;
+	ExceptionListeVide(string m = "Erreur : liste vide") { m_message = m; };
+};
+
+
+
+//classe exception index de liste
+class ExceptionIndex {
+public:
+	string m_message;
+	int m_index;
+	ExceptionIndex(string m = "Erreur d'index", int index = 0) {
+		m_message = m;
+		m_index = index;
+	};
+};
+
+
+
+//definition des methodes de la classe fraction
+
+
+
+template <typename T>
+liste_chainee<T>::liste_chainee() {
+	m_debut = NULL;
+}
+
+template <typename T>
+liste_chainee<T>::liste_chainee(const liste_chainee& l) {
+	noeud* temp_old = l.m_debut, * temp_new = new noeud;
+	m_debut = temp_new;
+	while (temp_old != NULL) {
+		temp_new->suivant = new noeud;
+		temp_new->valeur = temp_old->valeur;
+		temp_new = temp_new->suivant;
+		temp_old = temp_old->suivant;
+	}
+}
+
+template <typename T>
+liste_chainee<T>::~liste_chainee() {
+	noeud* temp;
+	while (m_debut != NULL) {
+		temp = m_debut->suivant;
+		delete m_debut;
+		m_debut = temp;
+	}
+}
+
+template <typename T>
+int liste_chainee<T>::front() {
+	if (m_debut != NULL) {
+		return m_debut->valeur;
+	}
+	else {
+		throw ExceptionListeVide("Erreur methode front : liste vide");
+	}
+}
+
+template <typename T>
+int liste_chainee<T>::back() {
+	noeud* temp = m_debut;
+	if (m_debut != NULL) {
+		while (temp->suivant != NULL) {
+			temp = temp->suivant;
+		}
+		return temp->valeur;
+	}
+	else {
+		throw ExceptionListeVide("Erreur methode back : liste vide");
+	}
+}
+
+template <typename T>
+void liste_chainee<T>::push_front(int elt) {
+	noeud* new_noeud = new noeud;
+	new_noeud->suivant = m_debut;
+	new_noeud->valeur = elt;
+	m_debut = new_noeud;
+}
+
+template <typename T>
+void liste_chainee<T>::push_back(int elt) {
+	noeud* new_noeud = new noeud, * temp = m_debut;
+	new_noeud->suivant = NULL;
+	new_noeud->valeur = elt;
+	if (m_debut == NULL) {
+		m_debut = new_noeud;
+	}
+	else {
+		while (temp->suivant != NULL) {
+			temp = temp->suivant;
+		}
+		temp->suivant = new_noeud;
+	}
+}
+
+template <typename T>
+void liste_chainee<T>::pop_front() {
+	noeud* temp;
+	if (m_debut != NULL) {
+		temp = m_debut->suivant;
+		delete m_debut;
+		m_debut = temp;
+	}
+	else {
+		throw ExceptionListeVide("Erreur methode pop_front : liste vide");
+	}
+}
+
+template <typename T>
+void liste_chainee<T>::pop_back() {
+	noeud* temp = m_debut;
+	if (m_debut == NULL) {
+		throw ExceptionListeVide("Erreur methode pop_back : liste vide");
+	}
+	else if (m_debut->suivant == NULL) {
+		m_debut = NULL;
+	}
+	else {
+		while (temp->suivant->suivant != NULL) {
+			temp = temp->suivant;
+		}
+		delete temp->suivant;
+		temp->suivant = NULL;
+	}
+}
+
+template <typename T>
+int liste_chainee<T>::size() {
+	int l_size = 0;
+	noeud* temp = m_debut;
+	while (temp != NULL) {
+		temp = temp->suivant;
+		l_size++;
+	}
+	return l_size;
+}
+
+template <typename T>
+void liste_chainee<T>::insert(int i, int elt) {
+	int l_size = size();
+	noeud* temp = m_debut, * new_noeud = new noeud;
+	if (i > l_size) {
+		throw ExceptionIndex("Erreur methode insert : la liste contient moins de "
+			+ to_string(i) + " elements", i);
+	}
+	else if (i < 1) {
+		throw ExceptionIndex("Erreur methode insert : saisir index superieur a 0", i);
+	}
+	else if (i == 1) {
+		new_noeud->suivant = m_debut->suivant;
+		m_debut = new_noeud;
+	}
+	else {
+		new_noeud->valeur = elt;
+		for (int j = 1; j < i - 1; j++) {
+			temp = temp->suivant;
+		}
+		new_noeud->suivant = temp->suivant->suivant;
+		temp->suivant = new_noeud;
+	}
+}
+
+template <typename T>
+void liste_chainee<T>::erase(int i) {
+	int l_size = size();
+	noeud* temp1, * temp2;
+	if (i > l_size) {
+		throw ExceptionIndex("Erreur methode erase : la liste contient moins de "
+			+ to_string(i) + " elements", i);
+	}
+	else if (i < 1) {
+		throw ExceptionIndex("Erreur methode insert : saisir index superieur a 0", i);
+	}
+	else if (i == 1) {
+		pop_front();
+	}
+	else {
+		temp1 = m_debut;
+		for (int j = 2; j < i; j++) {
+			temp1 = temp1->suivant;
+		}
+		temp2 = temp1->suivant;
+		temp1->suivant = temp1->suivant->suivant;
+		delete temp2;
+	}
+}
+
+template <typename T>
+bool liste_chainee<T>::empty() {
+	return (size() == 0);
+}
+
+template <typename T>
+void liste_chainee<T>::clear() {
+	int l_size = size();
+	for (int i = 0; i < l_size; i++) {
+		pop_front();
+	}
+}
+
+template <typename T>
+void liste_chainee<T>::remove(int elt) {
+	noeud* temp1, * temp2;
+	if (m_debut->valeur == elt) {
+		pop_front();
+	}
+	temp1 = m_debut;
+	while (temp1->suivant != NULL) {
+		if (temp1->suivant->valeur == elt) {
+			temp2 = temp1->suivant;
+			temp1->suivant = temp1->suivant->suivant;
+			delete temp2;
+		}
+		else {
+			temp1 = temp1->suivant;
+		}
+	}
+}
+
+template <typename T>
+noeud* sep_list(noeud* debut) {
+	noeud* lent = debut, * rapide = debut->suivant;
+	while (rapide != NULL and rapide->suivant != NULL) {
+		rapide = rapide->suivant->suivant;
+		lent = lent->suivant;
+	}
+	return lent;
+}
+
+template <typename T>
+noeud* merge(noeud* debut, noeud* fin) {
+	noeud* nouveau = new noeud, * temp = nouveau;
+	while (debut != NULL and fin != NULL) {
+		if (debut->valeur <= fin->valeur) {
+			temp->suivant = debut;
+			debut = debut->suivant;
+		}
+		else {
+			temp->suivant = fin;
+			fin = fin->suivant;
+		}
+		temp = temp->suivant;
+	}
+	if (debut != NULL) {
+		temp->suivant = debut;
+	}
+	else {
+		temp->suivant = fin;
+	}
+	return nouveau->suivant;
+}
+
+template <typename T>
+noeud* merge_sort(noeud* debut) {
+	noeud* fin, * sep, * gauche, * droite, * nouveau;
+	if (debut == NULL or debut->suivant == NULL) {
+		return debut;
+	}
+	sep = sep_list(debut);
+	fin = sep->suivant;
+	sep->suivant = NULL;
+
+	gauche = merge_sort(debut);
+	droite = merge_sort(fin);
+
+	nouveau = merge(gauche, droite);
+
+	return nouveau;
+}
+
+template <typename T>
+void liste_chainee<T>::sort() {
+	if (m_debut != NULL) {
+		m_debut = merge_sort(m_debut);
+	}
+}
+
+template <typename T>
+liste_chainee& liste_chainee<T>::operator=(const liste_chainee& l) {
+	noeud* temp_old = l.m_debut, * temp_new = new noeud;
+	if (this != &l) {
+		this->clear();
+		if (temp_old != NULL) {
+			m_debut = temp_new;
+			while (temp_old->suivant != NULL) {
+				temp_new->suivant = new noeud;
+				temp_new->valeur = temp_old->valeur;
+				temp_new = temp_new->suivant;
+				temp_old = temp_old->suivant;
+			}
+			temp_new->suivant = NULL;
+			temp_new->valeur = temp_old->valeur;
+		}
+	}
+	return *this;
+}
+
+ostream& operator<<(ostream& f, const liste_chainee& l) {
+	noeud* temp = l.m_debut;
+	if (temp != NULL) {
+		while (temp->suivant != NULL) {
+			f << temp->valeur << " -> ";
+			temp = temp->suivant;
+		}
+		f << temp->valeur;
+	}
+	return f;
+}
+
+liste_chainee operator+(const liste_chainee& l1, const liste_chainee& l2) {
+	liste_chainee l_new;
+	noeud* temp_l1 = l1.m_debut, * temp_l2 = l2.m_debut, * temp_lnew, * new_noeud;
+	if (l1.m_debut != NULL) {
+		l_new.push_front(l1.m_debut->valeur);
+		temp_lnew = l_new.m_debut;
+		temp_l1 = l1.m_debut->suivant;
+	}
+	else if (l2.m_debut != NULL) {
+		l_new.push_front(l2.m_debut->valeur);
+		temp_lnew = l_new.m_debut;
+		temp_l2 = l2.m_debut->suivant;
+	}
+	else {
+		return l_new;
+	}
+	while (temp_l1 != NULL) {
+		new_noeud = new noeud;
+		new_noeud->suivant = NULL;
+		new_noeud->valeur = temp_l1->valeur;
+		temp_lnew->suivant = new_noeud;
+		temp_lnew = new_noeud;
+		temp_l1 = temp_l1->suivant;
+	}
+	while (temp_l2 != NULL) {
+		new_noeud = new noeud;
+		new_noeud->suivant = NULL;
+		new_noeud->valeur = temp_l2->valeur;
+		temp_lnew->suivant = new_noeud;
+		temp_lnew = new_noeud;
+		temp_l2 = temp_l2->suivant;
+	}
+	return l_new;
+}
+
+//méthode permettant d'afficher le menu dans la fonction main
+void affichage_menu() {
+	cout << "Vous avez a votre disposition 10 listes vides numerotees de 1 a 10" << "\n\n";
+	cout << "0) arreter la saisie" << endl;
+	cout << "1) front : retourne le premier element de la liste" << endl;
+	cout << "2) back : retourne le dernier element de la liste" << endl;
+	cout << "3) push_front(elt) : ajoute un element au debut de la liste" << endl;
+	cout << "4) push_back(elt) : ajoute un element a la fin de la liste" << endl;
+	cout << "5) pop_front : supprime le premier element de la liste" << endl;
+	cout << "6) pop_back : supprimer le dernier element de la liste" << endl;
+	cout << "7) insert(i, elt) : ajoute un element a la ieme position dans la liste" << endl;
+	cout << "8) erase(i) : supprime le ieme element de la liste" << endl;
+	cout << "9) size : renvoie la taille de la liste" << endl;
+	cout << "10) empty : renvoie vrai si la liste est vide, faux sinon" << endl;
+	cout << "11) clear : vide la liste" << endl;
+	cout << "12) remove(elt) : supprime de la liste tous les elements egaux a elt" << endl;
+	cout << "13) sort : trie la liste par ordre croissant" << endl;
+	cout << "14) = : attribuer les valeurs d une autre liste a cette liste" << endl;
+	cout << "15) + : concatener deux listes" << endl;
+}
+
+
+
+#endif // _LISTE_CHAINEE_
