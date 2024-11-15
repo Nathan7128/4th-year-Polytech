98d64958da548077f4616e3c5c39dfdf50469eb0
Nathan7128 <nathan.talbot28@gmail.com>
2024-11-15 09:44:04 +0100

    revision tp noté mns

--- Methodes numeriques et simulation/TP6/Ex1.py
+++ Methodes numeriques et simulation/TP6/Ex1.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy.sparse as sp
 import time
 from scipy.sparse.linalg import spsolve
+import matplotlib.pyplot as plt
+
 
 
 # set t0
@@ -17,29 +19,43 @@ D = 2*(10**(-5))
 
 T = 36000
 
-def f(x, y) :
-    return 0
+# def f(x, y, t) :
+#     return np.zeros(np.shape(x))
 
-def g(x, y) :
-    return T_ext
+# def g(x, y, t) :
+#     return T_ext
 
-def u0(x) :
+def u0(x, y) :
     return T_ext + 15*np.sin(np.pi*x)*np.sin(np.pi*y)
 
-I = 1000
+I = 100
 
-N = 10
+N = 1000
 
 h = 1/I
 
 d_t = T/N
 
+sub_x = np.linspace(0, 1, I + 1)
+sub_y = np.linspace(0, 1, I + 1)
+sub_t = np.linspace(0, T, N + 1)
+
+bord_gauche = range(0, (I + 1)**2, I + 1)
+bord_droit = range(10, (I + 1)**2, I + 1)
+bord_bas = range(0, I + 1)
+bord_haut = range(I*(I + 1), (I + 1)**2)
+bords = list(bord_gauche) + list(bord_droit) + list(bord_bas) + list(bord_haut)
+
+sub_xy = np.array([(x, y) for y in sub_y for x in sub_x])
+
+lambd = (D*d_t)/(h**2)
+
+print("(d_t*D)/(h**2) = ", lambd)
+
 
 
 # Matrix
-v = 1/(h**2)
-
-diag_D1 = [-v*np.ones(I + 1), 2*v*np.ones(I + 1), -v*np.ones(I + 1)]
+diag_D1 = [-lambd*np.ones(I + 1), 2*lambd*np.ones(I + 1), -lambd*np.ones(I + 1)]
 
 off_set_D1 = [-1, 0, 1]
 
@@ -58,34 +74,40 @@ M[0, 0] = M[-1, -1] = 0
 
 D = sp.kron(D1, M) + sp.kron(M, D1)
 
-D[0, 0] = D[I, I] = D[-I - 1, -I - 1] = D[-1, -1] = 1
+D[bords] = 1
 
 
-F = np.zeros((I + 1)**2)
-G = np.zeros((I + 1)**2)
-U_sol = np.zeros((I + 1)**2)
+U_old = u0(sub_xy[:, 0], sub_xy[:, 1])
+U_new = np.zeros((I + 1)**2)
 
-for j in range(I + 1) :
-    xj = j*h
-    for i in range(I + 1) :
-        xi = i*h
+U_old[bords] = T_ext
+
+
+
+# Schéma d'Euler implicite
+for n in range (N) :
+    U_old[bords] = T_ext
+    U_new = spsolve(D, U_old)
+    U_old = U_new.copy()
+    
+    if n==0:
+        fig = plt.figure(figsize=(8,6))
+        im=plt.imshow(U_new.reshape(I+1,I+1),extent =[sub_x.min(), sub_x.max(), sub_y.min(), sub_y.max()])
+        plt.title("Température")
+        plt.colorbar()
+        plt.xlabel(u"x", fontsize=26)
+        plt.ylabel(u"y", fontsize=26, rotation=90)
+        plt.pause(0.01)
+    else:
+        if (n%(int(N/300)+1) == 0):
+            im.set_array(U_new.reshape(I+1,I+1))
+            plt.title(u"Equation de la chaleur 2D. t="+" %1.2f" %(n*d_t), fontsize=52)
+            plt.pause(0.01)
         
-        F[i + j*(I + 1)] = f(xi, xj)
         
-        U_sol[i + j*(I + 1)] = u(xi, xj)
         
-        if i == 0 or i == I or j == 0 or j == I:
-            G[i + j*(I + 1)] = g(xi, xj)
-
-
-
-# Show result
-U = spsolve(D, F + G)
-norme = np.linalg.norm(U - U_sol, np.inf)
-print("Norme :", norme)
-
-
-
-# print time spent
-print("elapsed time %f " %(time.time() - t0))
+        
+        
+        
+        
 
