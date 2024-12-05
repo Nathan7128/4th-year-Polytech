e3e33eef9a1add77917121004de70e2c0bdcf4ac
Nathan Talbot <nathan.talbot28@gmail.com>
2024-12-05 17:15:22 +0100

    tp qt c++

---
+++ C++/Td_QT/Calculatrice/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Calculatrice_autogen/include/ui_mainwindow.h
@@ -0,0 +1,148 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QTextBrowser *textBrowser;
+    QPushButton *Boutton1;
+    QPushButton *Boutton2;
+    QPushButton *Boutton3;
+    QPushButton *Boutton4;
+    QPushButton *Boutton5;
+    QPushButton *Boutton6;
+    QPushButton *Boutton7;
+    QPushButton *Boutton8;
+    QPushButton *Boutton9;
+    QPushButton *Boutton5_2;
+    QPushButton *Boutton4_2;
+    QPushButton *Boutton9_2;
+    QPushButton *Boutton5_3;
+    QPushButton *Boutton6_2;
+    QPushButton *Boutton7_2;
+    QPushButton *Boutton5_4;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName("textBrowser");
+        textBrowser->setGeometry(QRect(420, 120, 171, 41));
+        Boutton1 = new QPushButton(centralwidget);
+        Boutton1->setObjectName("Boutton1");
+        Boutton1->setGeometry(QRect(490, 180, 31, 31));
+        Boutton2 = new QPushButton(centralwidget);
+        Boutton2->setObjectName("Boutton2");
+        Boutton2->setGeometry(QRect(550, 180, 31, 31));
+        Boutton3 = new QPushButton(centralwidget);
+        Boutton3->setObjectName("Boutton3");
+        Boutton3->setGeometry(QRect(490, 230, 31, 31));
+        Boutton4 = new QPushButton(centralwidget);
+        Boutton4->setObjectName("Boutton4");
+        Boutton4->setGeometry(QRect(430, 230, 31, 31));
+        Boutton5 = new QPushButton(centralwidget);
+        Boutton5->setObjectName("Boutton5");
+        Boutton5->setGeometry(QRect(430, 280, 31, 31));
+        Boutton6 = new QPushButton(centralwidget);
+        Boutton6->setObjectName("Boutton6");
+        Boutton6->setGeometry(QRect(490, 280, 31, 31));
+        Boutton7 = new QPushButton(centralwidget);
+        Boutton7->setObjectName("Boutton7");
+        Boutton7->setGeometry(QRect(550, 280, 31, 31));
+        Boutton8 = new QPushButton(centralwidget);
+        Boutton8->setObjectName("Boutton8");
+        Boutton8->setGeometry(QRect(550, 230, 31, 31));
+        Boutton9 = new QPushButton(centralwidget);
+        Boutton9->setObjectName("Boutton9");
+        Boutton9->setGeometry(QRect(430, 180, 31, 31));
+        Boutton5_2 = new QPushButton(centralwidget);
+        Boutton5_2->setObjectName("Boutton5_2");
+        Boutton5_2->setGeometry(QRect(370, 280, 31, 31));
+        Boutton4_2 = new QPushButton(centralwidget);
+        Boutton4_2->setObjectName("Boutton4_2");
+        Boutton4_2->setGeometry(QRect(370, 230, 31, 31));
+        Boutton9_2 = new QPushButton(centralwidget);
+        Boutton9_2->setObjectName("Boutton9_2");
+        Boutton9_2->setGeometry(QRect(370, 180, 31, 31));
+        Boutton5_3 = new QPushButton(centralwidget);
+        Boutton5_3->setObjectName("Boutton5_3");
+        Boutton5_3->setGeometry(QRect(430, 330, 31, 31));
+        Boutton6_2 = new QPushButton(centralwidget);
+        Boutton6_2->setObjectName("Boutton6_2");
+        Boutton6_2->setGeometry(QRect(490, 330, 31, 31));
+        Boutton7_2 = new QPushButton(centralwidget);
+        Boutton7_2->setObjectName("Boutton7_2");
+        Boutton7_2->setGeometry(QRect(550, 330, 31, 31));
+        Boutton5_4 = new QPushButton(centralwidget);
+        Boutton5_4->setObjectName("Boutton5_4");
+        Boutton5_4->setGeometry(QRect(370, 330, 31, 31));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 25));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        Boutton1->setText(QCoreApplication::translate("MainWindow", "8", nullptr));
+        Boutton2->setText(QCoreApplication::translate("MainWindow", "9", nullptr));
+        Boutton3->setText(QCoreApplication::translate("MainWindow", "5", nullptr));
+        Boutton4->setText(QCoreApplication::translate("MainWindow", "4", nullptr));
+        Boutton5->setText(QCoreApplication::translate("MainWindow", "1", nullptr));
+        Boutton6->setText(QCoreApplication::translate("MainWindow", "2", nullptr));
+        Boutton7->setText(QCoreApplication::translate("MainWindow", "3", nullptr));
+        Boutton8->setText(QCoreApplication::translate("MainWindow", "6", nullptr));
+        Boutton9->setText(QCoreApplication::translate("MainWindow", "7", nullptr));
+        Boutton5_2->setText(QCoreApplication::translate("MainWindow", "*", nullptr));
+        Boutton4_2->setText(QCoreApplication::translate("MainWindow", "-", nullptr));
+        Boutton9_2->setText(QCoreApplication::translate("MainWindow", "+", nullptr));
+        Boutton5_3->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        Boutton6_2->setText(QCoreApplication::translate("MainWindow", "8", nullptr));
+        Boutton7_2->setText(QCoreApplication::translate("MainWindow", "=", nullptr));
+        Boutton5_4->setText(QCoreApplication::translate("MainWindow", "/", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
